24d2dee98e1ff91e16211f70dc6f5b6d1a5a2916
Jyu2h11 <lechelle.heloise@gmail.com>
2022-08-14 03:19:52 +0200

    icones magies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { MagicComponent } from './magic/magic.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    MagicComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'magic', component: MagicComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
