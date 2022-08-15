b33654cdbbbe6cbbf47ab0c31a4bb7ecb468a53b
Jyu2h11 <lechelle.heloise@gmail.com>
2022-08-15 18:16:34 +0200

    resolve prob

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -16,32 +16,20 @@ export class TopBarComponent implements OnInit {
   getInfo() {
     const infos = document.getElementById('infos');
     const mecas = document.getElementById('mecas');
-    const catInfo = document.getElementById('cat1');
-    const catMeca = document.getElementById('cat2');
     
-    if(infos !== null && mecas !== null && catInfo !== null && catMeca !== null) {
+    if(infos !== null && mecas !== null) {
       infos.style.display = 'block';
       mecas.style.display = 'none';
-      catInfo.style.background = '#eebb14';
-      catInfo.style.color = 'black';
-      catMeca.style.background = 'black';
-      catMeca.style.color = '#eebb14';
     }
   }
 
   getMeca() {
     const infos = document.getElementById('infos');
     const mecas = document.getElementById('mecas');
-    const catInfo = document.getElementById('cat1');
-    const catMeca = document.getElementById('cat2');
     
-    if(infos !== null && mecas !== null && catInfo !== null && catMeca !== null) {
+    if(infos !== null && mecas !== null) {
       infos.style.display = 'none';
       mecas.style.display = 'block';
-      catMeca.style.background = '#eebb14';
-      catMeca.style.color = 'black';
-      catInfo.style.background = 'black';
-      catInfo.style.color = '#eebb14';
     }
   }
 
