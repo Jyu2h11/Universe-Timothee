320480d6a04d297c23ab6935633b84c60ec97289
Blackymero <sidadam@hotmail.fr>
2022-08-19 14:20:47 +0200

    début de la page des postures !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { MagicComponent } from './magic/magic.component';
-import { HomeComponent } from './home/home.component';
+import { PostureComponent } from './posture/posture.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     MagicComponent,
-    HomeComponent
+    PostureComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent},
       { path: 'magic', component: MagicComponent }
     ])
   ],
---
+++ src/app/posture/posture.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posture',
+  templateUrl: './posture.component.html',
+  styleUrls: ['./posture.component.css']
+})
+export class PostureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
